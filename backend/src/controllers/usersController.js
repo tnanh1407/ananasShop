8f82ff0869acbac542145166016d0cbe55c98483
Tran Ngoc Anh <tnanh.vn@gmail.com>
2025-11-20 20:32:44 +0700

    completed user

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -1,6 +1,7 @@
 import User from "../models/Users.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -8,6 +9,7 @@ const generateToken = (id) => {
   });
 };
 
+// FUNCTION REGISTER
 export const registerUserController = async (req, res) => {
   const { fullname, email, password, username } = req.body;
 
@@ -63,6 +65,7 @@ export const registerUserController = async (req, res) => {
   }
 };
 
+// function login
 export const loginUserController = async (req, res) => {
   const { accountIdentifier, password } = req.body;
 
@@ -114,7 +117,124 @@ export const loginUserController = async (req, res) => {
     });
   } catch (e) {
     // Sửa: Thay 'error' bằng 'e'
-    console.error("Lỗi đăng nhập : ", e);
+    console.error("Error Function Login : ", e);
     return res.status(500).json({ message: "Đã xảy ra lỗi hệ thống" });
+  }
+};
+
+// CRUD
+export const getAllUsersController = async (req, res) => {
+  try {
+    const user = await User.find({});
+    res.status(200).json({
+      message: "Lấy danh sách người dùng thành công !",
+      user: user,
+      count: user.length,
+    });
+  } catch (error) {
+    console.log("Error Function GetAllUsers : ", error);
+    return res.status(500).json({ message: "Đã xảy ra lỗi hệ thống" });
+  }
+};
+
+export const getByIdUserController = async (req, res) => {
+  try {
+    // 1 . check xem người dùng đã tồn tại chưa
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) {
+      res.status(404).json({ message: ` Không tồn tại trong CSDL` });
+    }
+    res.status(200).json({
+      message: `Lấy người dùng với _id ${user._id}  thành công !`,
+      user: user,
+    });
+  } catch (error) {
+    console.log("Error Function getByIdUser : ", error);
+    return res.status(500).json({ message: "Đã xảy ra lỗi hệ thống" });
+  }
+};
+
+export const updateUserController = async (req, res) => {
+  // 1. lấy ID và URL Parameters và dữ liệu cập nhật từ body
+  const { id } = req.params;
+  const updateData = req.body;
+
+  // 2. Check Xem có dữ liệu nào được cập nhật hay không ?
+
+  if (Object.keys(updateData).length === 0) {
+    return res
+      .status(400)
+      .json({ message: "Không có dữ liệu nào cung cấp để cập nhật !" });
+  }
+  try {
+    // 3. Kiểm tra xem dữ liệu id có hợp  lệ hay không ?
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID nguời dùng không hợp lệ" });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    //  4. Kiểm tra sự tồn tại của người dùng
+
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy người dùng để cập nhật !" });
+    }
+
+    // 5. trả về phản hồi thành công
+    return res.status(200).json({
+      message: "Cập nhật thông tin người dùng thành công",
+      user: updatedUser,
+    });
+  } catch (e) {
+    if (error.name === "ValidationError") {
+      return res
+        .status(400)
+        .json({ message: "Dữ liệu cập nhật không hợp lệ", error: e.message });
+    }
+    console.error("Lỗi người dùng cập nhật ", e);
+    return res.status(500).json({ message: "Đã xảy ra lỗi hệ thống " });
+  }
+};
+
+// 4. DELETE (Xóa sản phẩm theo ID)
+export const deleteUserController = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy người dùng để xóa " });
+    }
+    res
+      .status(200)
+      .json({ message: "Xoá sản phẩm thành công ", user: deletedUser });
+  } catch (error) {
+    res.status(500).json({
+      message: "Lỗi sản phẩm",
+      error: `Lỗi function deleteUserController ${error.message}`,
+    });
+  }
+};
+
+export const logoutUserController = async (req, res) => {
+  try {
+    res.cookie("jwt", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    return res.status(200).json({
+      message: "Đăng xuất thành công. Token đã được xóa khỏi Cookie.",
+    });
+  } catch (error) {
+    console.error("Lỗi đăng xuất:", error);
+    return res
+      .status(500)
+      .json({ message: "Đã xảy ra lỗi hệ thống khi xử lý đăng xuất." });
   }
 };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,20 @@
 import express from "express";
 import {
+  deleteUserController,
+  getAllUsersController,
+  getByIdUserController,
   loginUserController,
+  logoutUserController,
   registerUserController,
+  updateUserController,
 } from "../controllers/usersController.js";
 
 const router = express.Router();
 router.post("/register", registerUserController);
 router.post("/login", loginUserController);
-
+router.get("/", getAllUsersController);
+router.get("/:id", getByIdUserController);
+router.delete("/:id", deleteUserController);
+router.patch("/:id", updateUserController);
+router.post("/logout", logoutUserController);
 export default router;
